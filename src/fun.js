8a7a0c8ca49169ac1299ed54cc3f5caed157442d
Rishabh Sharma <rishabhsharma@Rishabhs-MacBook-Air.local>
2024-03-15 12:21:11 +0530

    add project

---
+++ src/fun.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function user()
+{
+    return(
+        <div>
+              <label for="username">Username</label>
+                <input type="text" id="username" placeholder="rishabh" name="username" required></input>
+        </div>
+    )
+}
+
+export default user
+
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Data from './App';
+import User from './fun';
+
+ReactDOM.render(
+  <React.Fragment>
+    <Data/>
+    <User/>
+
+
+  </React.Fragment>
+  ,document.getElementById("root")
+)
